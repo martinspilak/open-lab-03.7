b325fb6386200ea09e413b4580a3eacffd18d138
martinspilak <martinecko24@gmail.com>
2019-10-19 17:07:06 +0200

    open-lab-03.7

---
+++ open-lab-03.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace open_lab_03._7
+{
+    class Program
+    {
+        public static bool EqualSlices(int total, int people, int each)
+        {
+            return (true);
+        }
+
+        static void Main(string[] args)
+        {
+            int total = Convert.ToInt32(Console.ReadLine());
+            int people = Convert.ToInt32(Console.ReadLine());
+            int each = Convert.ToInt32(Console.ReadLine());
+            if (people * each <= total)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+        }
+    }
+}
